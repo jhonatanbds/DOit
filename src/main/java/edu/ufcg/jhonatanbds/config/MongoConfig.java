6f9641b945af88901a53eaa4abc3748329acad0f
Jhonatan Batista <jhonatan.silva@ccc.ufcg.edu.br>
2017-02-15 22:54:28 -0200

    acho q agora foi

--- src/main/java/edu/ufcg/jhonatanbds/config/MongoConfig.java
+++ src/main/java/edu/ufcg/jhonatanbds/config/MongoConfig.java
@@ -1,19 +1,24 @@
 package edu.ufcg.jhonatanbds.config;
 
+import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import com.mongodb.*;
 import edu.ufcg.jhonatanbds.converter.ToDoWriterConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.data.mongodb.core.convert.CustomConversions;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
+import static java.util.Collections.singletonList;
 
 @Configuration
 @EnableMongoRepositories(basePackages = "edu.ufcg.jhonatanbds.dao")
@@ -27,8 +32,10 @@ public class MongoConfig extends AbstractMongoConfiguration {
     }
 
     @Override
+    @Bean
     public Mongo mongo() throws Exception {
-        return new MongoClient("ds153729.mlab.com", 53729);
+        return new MongoClient(singletonList(new ServerAddress("ds153729.mlab.com", 53729)),
+                singletonList(MongoCredential.createCredential("heroku_zv2trqp4", "heroku_zv2trqp4", "9dbsre2qaqf7a4q1fc3a5k8n58".toCharArray())));
     }
 
     @Override
